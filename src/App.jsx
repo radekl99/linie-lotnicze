7d6910eced1762e452a981d5c1cbbafdb7f4d745
Radoslaw Laszek <radek.laszek1999@gmail.com>
2022-03-09 13:04:58 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <h1>Wyszukiwarka lotów</h1>
+        {/* FORM */}
+        {/* LISTA LOTÓW */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
